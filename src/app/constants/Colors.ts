ad4fd371ebc1c222bd5ab5163ca302508b1cc52b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-29 13:51:28 +0100

    Update Colors.ts

---
+++ src/app/constants/Colors.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: '#7D7AFF',
+  secondary: '#your-secondary-color',
+  background: '#F2F5FA',
+  textPrimary: '#000000',
+  textSecondary: '#838D95',
+  success: '#28a745',
+  error: '#dc3545',
+  warning: '#ffc107',
+};
