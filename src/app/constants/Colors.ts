3c08c9eb757ca80bb81e5e05bc8be28673b1f33e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-29 13:54:42 +0100

    Update Colors.ts

--- src/app/constants/Colors.ts
+++ src/app/constants/Colors.ts
@@ -1,10 +1,21 @@
+import Constants from 'expo-constants';
+
+const primaryColor = Constants.expoConfig?.extra?.primaryColor || '#7D7AFF';
+const secondaryColor = Constants.expoConfig?.extra?.secondaryColor || '#1E20241A';
+const backgroundColor = Constants.expoConfig?.extra?.backgroundColor || '#F2F5FA';
+const textPrimaryColor = Constants.expoConfig?.extra?.textPrimaryColor || '#000000';
+const textSecondaryColor = Constants.expoConfig?.extra?.textSecondaryColor || '#838D95';
+const successColor = Constants.expoConfig?.extra?.successColor || '#28a745';
+const errorColor = Constants.expoConfig?.extra?.errorColor || '#dc3545';
+const warningColor = Constants.expoConfig?.extra?.warningColor || '#ffc107';
+
 export default {
-  primary: '#7D7AFF',
-  secondary: '#your-secondary-color',
-  background: '#F2F5FA',
-  textPrimary: '#000000',
-  textSecondary: '#838D95',
-  success: '#28a745',
-  error: '#dc3545',
-  warning: '#ffc107',
+  primary: primaryColor,
+  secondary: secondaryColor,
+  background: backgroundColor,
+  textPrimary: textPrimaryColor,
+  textSecondary: textSecondaryColor,
+  success: successColor,
+  error: errorColor,
+  warning: warningColor,
 };
