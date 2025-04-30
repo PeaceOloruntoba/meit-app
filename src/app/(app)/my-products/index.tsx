25dc918350cfb680c621fce3787028ece3bf7657
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-30 12:25:18 +0100

    Update index.tsx

--- src/app/(app)/my-products/index.tsx
+++ src/app/(app)/my-products/index.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { View, FlatList, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import ProductCard from "../../components/UI/ProductCard";
+import useProduct from "../../hooks/useProduct";
+import { Link } from "expo-router";
+import { ActivityIndicator, Text } from "react-native";
+import Colors from "../../constants/Colors";
 
-export default function index() {
+const MyProductsPage = () => {
+  const { loading, error, products, getUserProducts } = useProduct();
+
+  useEffect(() => {
+    getUserProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-white">
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className="flex-1 justify-center items-center bg-white p-4">
+        <Text className="text-red-500 text-center">{error}</Text>
+      </View>
+    );
+  }
+
   return (
-    <div>index</div>
-  )
-}
+    <View className="flex-1 bg-white p-4">
+      <View className="flex-row justify-end mb-4">
+        <Link href="/add-product" className="bg-black rounded-full p-3">
+          <Feather name="plus" size={24} color="white" />
+        </Link>
+      </View>
+      {products.length > 0 ? (
+        <FlatList
+          data={products}
+          keyExtractor={(item) => item.id!}
+          renderItem={({ item }) => <ProductCard {...item} />}
+          showsVerticalScrollIndicator={false}
+        />
+      ) : (
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-gray-500">No products added yet.</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default MyProductsPage;
