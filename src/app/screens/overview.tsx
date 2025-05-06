e2a509572776256dd378a6d03f0f1253c6f4c595
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 15:29:58 +0100

    Create overview.tsx

---
+++ src/app/screens/overview.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function overview() {
+  return (
+    <View>
+      <Text>overview</Text>
+    </View>
+  );
+}
