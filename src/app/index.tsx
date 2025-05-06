c1b97d4f79439ad8594ee5b1015ab8bfd9c1bb27
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 17:37:02 +0100

    ok

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -30,7 +30,6 @@ export default function Page() {
       <Header />
       <Content />
       <BottomNav items={navigationItems} />
-      <Footer />
     </View>
   );
 }
@@ -95,22 +94,6 @@ function Header() {
             Pricing
           </Link>
         </View>
-      </View>
-    </View>
-  );
-}
-
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
-  return (
-    <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
-    >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
-        </Text>
       </View>
     </View>
   );
