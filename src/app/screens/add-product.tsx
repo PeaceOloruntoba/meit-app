29d9442266d069061a264e76127d2ce99281ac6e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 15:31:28 +0100

    Create add-product.tsx

---
+++ src/app/screens/add-product.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function addproduct() {
+  return (
+    <View>
+      <Text>addproduct</Text>
+    </View>
+  );
+}
