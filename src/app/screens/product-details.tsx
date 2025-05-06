ef6b3a1b6be697211d8e0ec35ff7c2e36ffa0c8c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 15:31:10 +0100

    Create product-details.tsx

---
+++ src/app/screens/product-details.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function productdetails() {
+  return (
+    <View>
+      <Text>productdetails</Text>
+    </View>
+  );
+}
