93f19ea0a5fc8203825f491e5309ec4a14d394b7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 16:18:36 +0100

    asaa

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function profile() {
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center">
       <Text>profile</Text>
-    </View>
+    </SafeAreaView>
   );
 }
