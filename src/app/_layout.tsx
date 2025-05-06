2eb20df3574088f0737d1c2c684107821b1bd9ad
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 15:46:54 +0100

    more more more

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,18 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        {/* <AuthProvider> */}
+        <StatusBar style="dark" />
+        <Slot />
+        {/* </AuthProvider> */}
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
+  );
 }
