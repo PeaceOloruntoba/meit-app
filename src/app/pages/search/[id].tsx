6f84756f1b4313d472e15d2969cb6a3b97bf5988
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 19:38:37 +0100

    ok

---
+++ src/app/pages/search/[id].tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
+import Colors from "@/constants/Colors";
+
+const SearchDetailsScreen = () => {
+  const router = useRouter();
+
+  return (
+    <View className="flex-1 justify-center items-center bg-primary">
+      <Text className="text-textPrimary text-3xl font-bold mb-4">
+        Welcome to Meit App: Search Details
+      </Text>
+      <ActivityIndicator size="large" color={Colors.primary} />
+    </View>
+  );
+};
+
+export default SearchDetailsScreen;
