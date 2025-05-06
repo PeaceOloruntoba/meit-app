0565fe25934423c767e8c24556aae29304adec46
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 20:44:00 +0100

    Update index.tsx

--- src/app/pages/profile/index.tsx
+++ src/app/pages/profile/index.tsx
@@ -1,17 +1,74 @@
 import React from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { useRouter } from "expo-router";
 import Colors from "@/constants/Colors";
+import { Feather } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
   const router = useRouter();
+  const [searchText, setSearchText] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSearch = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      console.log("Searching for:", searchText);
+    }, 1500);
+  };
+
+  const handleFilter = () => {
+    console.log("Navigating to filters");
+  };
+
+  const navigateToDetails = () => {
+    router.push("/pages/profile/edit-profile");
+  };
 
   return (
-    <View className="flex-1 justify-center items-center bg-primary">
-      <Text className="text-textPrimary text-3xl font-bold mb-4">
-        Welcome to Meit App: Profile
-      </Text>
-      <ActivityIndicator size="large" color={Colors.primary} />
+    <View className="flex-1 bg-background pt-20">
+      <View className="px-4 pt-6">
+        <View className="flex flex-row items-center space-x-2">
+          <View className="flex-1 bg-white rounded-lg shadow-sm">
+            <TextInput
+              className="w-full p-3 text-lg text-textPrimary"
+              placeholder="Suche..."
+              value={searchText}
+              onChangeText={setSearchText}
+              onSubmitEditing={handleSearch}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={handleFilter}
+            className="bg-black rounded-lg p-3 ml-4 shadow-md"
+          >
+            <Feather name="filter" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        {loading && (
+          <View className="mt-8 items-center">
+            <ActivityIndicator size="large" color={Colors.primary} />
+            <Text className="mt-2 text-textSecondary">Searching...</Text>
+          </View>
+        )}
+
+        <View className="mt-10 px-4">
+          <TouchableOpacity
+            onPress={navigateToDetails}
+            className="bg-gray-200 p-4 rounded-lg mb-4 items-center"
+          >
+            <Text className="text-lg font-bold">Go to Search Details (ID)</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
