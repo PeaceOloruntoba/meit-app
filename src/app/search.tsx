a96afc16b52de07a22e604da2cdfcfd8985658d0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 16:18:58 +0100

    Update search.tsx

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function search() {
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center">
       <Text>search</Text>
-    </View>
+    </SafeAreaView>
   );
 }
