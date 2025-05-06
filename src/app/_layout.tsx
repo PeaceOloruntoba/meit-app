f8a35da3ec5c756907b6b93d9b2b375b83832e91
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 19:23:24 +0100

    aaa

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,14 +6,35 @@ import Colors from "@/constants/Colors";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Toaster } from "sonner-native";
+import BottomNav from "@/components/BottomNav";
+import { Feather } from "@expo/vector-icons";
 
 export default function Layout() {
+  const navigationItems = [
+    {
+      href: "/search",
+      icon: <Feather name="search" size={24} color={Colors.primary} />,
+    },
+    {
+      href: "/products",
+      icon: <Feather name="grid" size={24} color={Colors.primary} />,
+    },
+    {
+      href: "/rentals",
+      icon: <Feather name="list" size={24} color={Colors.primary} />,
+    },
+    {
+      href: "/profile",
+      icon: <Feather name="user" size={24} color={Colors.primary} />,
+    },
+  ];
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={{ backgroundColor: Colors.background }}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         {/* <AuthProvider> */}
         <StatusBar style="dark" backgroundColor={Colors.background} />
         <Slot />
+        <BottomNav items={navigationItems} />
         <Toaster
           toastOptions={{
             style: { backgroundColor: "#F2F5FA" },
