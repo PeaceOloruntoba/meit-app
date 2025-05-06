4aa213e8e55d7bf8396f086ffde2ff4f02430495
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 14:32:00 +0100

    master

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,6 @@
 import "../global.css";
 import { Slot } from "expo-router";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { AuthProvider } from "./hooks/useAuth";
-import Colors from "./constants/Colors";
-import { StatusBar } from "expo-status-bar";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout() {
-  return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <AuthProvider>
-          <StatusBar style="dark" backgroundColor={Colors.background} />
-          <Slot />
-        </AuthProvider>
-      </GestureHandlerRootView>
-    </SafeAreaProvider>
-  );
+export default function Layout() {
+  return <Slot />;
 }
--- src/app/components/Notifications/NotificationItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotificationItem() {
-  return (
-    <div>NotificationItem</div>
-  )
-}
