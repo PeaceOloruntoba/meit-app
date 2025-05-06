49afbc0169436d0be964361d3c70d0e5242d2b91
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-06 16:30:50 +0100

    Update BottomNav.tsx

--- src/app/components/BottomNav.tsx
+++ src/app/components/BottomNav.tsx
@@ -1,11 +1,14 @@
+// ./components/BottomNav.tsx
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { Link, usePathname, useRouter } from "expo-router";
 import Colors from "../../constants/Colors";
+import { Feather } from "@expo/vector-icons";
 
 interface BottomNavItem {
   href: string;
-  icon?: React.ReactNode;
+  icon: React.ReactNode;
+  activeIcon?: React.ReactNode;
 }
 
 interface BottomNavProps {
@@ -18,19 +21,27 @@ const BottomNav: React.FC<BottomNavProps> = ({ items }) => {
 
   return (
     <View className="flex-row justify-around items-center bg-white border-t border-gray-200 py-2">
-      {items.map((item) => (
-        <TouchableOpacity
-          key={item.href}
-          onPress={() => router.push(item.href)}
-          className={`flex-1 items-center py-2`}
-        >
-          {item.icon && (
-            <View className="mb-1">
-              {React.cloneElement(item.icon as React.ReactElement)}
-            </View>
-          )}
-        </TouchableOpacity>
-      ))}
+      {items.map((item) => {
+        const isActive = pathname?.startsWith(item.href);
+        const iconColor = isActive ? Colors.primary : Colors.textPrimary;
+
+        const coloredIcon = React.cloneElement(
+          item.icon as React.ReactElement,
+          {
+            color: iconColor,
+          }
+        );
+
+        return (
+          <TouchableOpacity
+            key={item.href}
+            onPress={() => router.push(item.href)}
+            className={`flex-1 items-center py-2`}
+          >
+            <View className="mb-1">{coloredIcon}</View>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
