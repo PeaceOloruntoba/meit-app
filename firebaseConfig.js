8e32e3c6d1efff0616b250fede85e3c8c0d0c677
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-07 18:16:38 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP-uhwWxFg35xovNaTHKN5CsUJ9xCdebk", // Replace with your actual API key
+  apiKey: "AIzaSyBP-uhwWxFg35xovNaTHKN5CsUJ9xCdebk",
   authDomain: "meit-2e0f1.firebaseapp.com",
   projectId: "meit-2e0f1",
   storageBucket: "meit-2e0f1.firebasestorage.app",
   messagingSenderId: "273927954366",
   appId: "1:273927954366:android:b3a50cb29c79311717e3ef",
-  // databaseURL: "YOUR_DATABASE_URL", // If you plan to use Realtime Database
 };
 
 const app = initializeApp(firebaseConfig);
