003d5fce5d73be848ac5b147830672cc6f79517e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-07 15:02:36 +0100

    Update index.tsx

--- src/app/pages/profile/index.tsx
+++ src/app/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 
@@ -7,38 +7,38 @@ const ProfileScreen = () => {
   // const { user, loading, error, logout } = useAuth();
   const router = useRouter();
 
-//   if (loading) {
-//     return (
-//       <View className="flex-1 justify-center items-center">
-//         <Text className="text-lg">Profil wird geladen...</Text>
-//       </View>
-//     );
-//   }
-// 
-//   if (error) {
-//     return (
-//       <View className="flex-1 justify-center items-center">
-//         <Text className="text-red-500 text-lg">
-//           Fehler beim Laden des Profils: {error}
-//         </Text>
-//       </View>
-//     );
-//   }
-// 
-//   if (!user) {
-//     return (
-//       <View className="flex-1 justify-center items-center">
-//         <Text className="text-lg">Nicht angemeldet.</Text>
-//       </View>
-//     );
-//   }
+  //   if (loading) {
+  //     return (
+  //       <View className="flex-1 justify-center items-center">
+  //         <Text className="text-lg">Profil wird geladen...</Text>
+  //       </View>
+  //     );
+  //   }
+  //
+  //   if (error) {
+  //     return (
+  //       <View className="flex-1 justify-center items-center">
+  //         <Text className="text-red-500 text-lg">
+  //           Fehler beim Laden des Profils: {error}
+  //         </Text>
+  //       </View>
+  //     );
+  //   }
+  //
+  //   if (!user) {
+  //     return (
+  //       <View className="flex-1 justify-center items-center">
+  //         <Text className="text-lg">Nicht angemeldet.</Text>
+  //       </View>
+  //     );
+  //   }
 
   return (
     <View className="flex-1 bg-[#F2F5FA] p-4 pt-20 flex-col justify-between">
       <View>
         <TouchableOpacity
           onPress={() => router.push("/edit-profile")}
-          style={styles.listItem}
+          className="flex-row items-center justify-between px-5 py-4 bg-white mb-1"
         >
           <Text className="text-lg text-gray-800">Profil bearbeiten</Text>
           <Feather name="chevron-right" size={20} color="#888" />
@@ -48,7 +48,7 @@ const ProfileScreen = () => {
           onPress={() => {
             /* Handle Impressum navigation */
           }}
-          style={styles.listItem}
+          className="flex-row items-center justify-between px-5 py-4 bg-white mb-1"
         >
           <Text className="text-lg text-gray-800">Impressum</Text>
           <Feather name="chevron-right" size={20} color="#888" />
@@ -58,7 +58,7 @@ const ProfileScreen = () => {
           onPress={() => {
             /* Handle Rechtliches navigation */
           }}
-          style={styles.listItem}
+          className="flex-row items-center justify-between px-5 py-4 bg-white mb-1"
         >
           <Text className="text-lg text-gray-800">Rechtliches</Text>
           <Feather name="chevron-right" size={20} color="#888" />
@@ -66,7 +66,7 @@ const ProfileScreen = () => {
       </View>
 
       <View>
-        <TouchableOpacity style={styles.logoutButton}>
+        <TouchableOpacity className="flex-row items-center justify-between px-5 py-4 bg-white border border-[#d32f2f] mb-1 rounded-md">
           <Text className="text-lg text-red-600">Abmelden</Text>
           <Feather name="chevron-right" size={20} color="#d32f2f" />
         </TouchableOpacity>
@@ -75,7 +75,7 @@ const ProfileScreen = () => {
           onPress={() => {
             /* Handle Account löschen */
           }}
-          style={styles.deleteAccountButton}
+          className="flex-row items-center justify-between px-5 py-4 bg-[#d32f2f] mt-3 rounded-md"
         >
           <Text className="text-lg text-white">Account löschen</Text>
           <Feather name="chevron-right" size={20} color="#fff" />
@@ -84,38 +84,5 @@ const ProfileScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  listItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    backgroundColor: "#fff",
-    marginBottom: 1,
-  },
-  logoutButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    backgroundColor: "#fff",
-    borderWidth: 1,
-    borderColor: "#d32f2f",
-    marginBottom: 6,
-    borderRadius: 10,
-  },
-  deleteAccountButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    backgroundColor: "#d32f2f",
-    marginTop: 12,
-  },
-});
 
 export default ProfileScreen;
