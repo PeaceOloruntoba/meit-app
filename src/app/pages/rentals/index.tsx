7cd50855c5b912e2bae50ab60c937553c0b40cf6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-10 14:54:12 +0100

    Update index.tsx

--- src/app/pages/rentals/index.tsx
+++ src/app/pages/rentals/index.tsx
@@ -61,13 +61,13 @@ const RentalScreen = () => {
     }
   };
 
-  // if (loading) {
-  //   return (
-  //     <View className="flex-1 bg-background justify-center items-center">
-  //       <ActivityIndicator size="large" />
-  //     </View>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <View className="flex-1 bg-background justify-center items-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   if (error) {
     return (
